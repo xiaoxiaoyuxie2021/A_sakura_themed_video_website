3f85aa2f1f0a9a8c3d6c2d25acc33aee8dab8373
沐咲汐 <xiaoxiaoyuxie2021@users.noreply.github.com>
2025-12-28 14:05:58 +0800

    略微调整了布局

--- js/data.js
+++ js/data.js
@@ -12,8 +12,8 @@ const categories = [
 
 // 视频数据
 const videos = [
-  {title:"【天哥版】最春哥", file:'1.mp4',thumb:"images/vim/1.jpg",plays:"2.3万"},
-  {title:"春日序曲",thumb:"https://source.unsplash.com/random/140x80?spring",url:"https://www.w3schools.com/html/mov_bbb.mp4",plays:"5.1万"},
+  {title:"视频测试",thumb:"缩略图",plays:"0"},
+  {title:"春日序曲",thumb:"缩略图",plays:"0"},
 ];
 
 
